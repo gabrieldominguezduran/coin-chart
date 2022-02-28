505f74ef350575e4cad097cd31e5d4c7530f541b
gabrieldominguezduran <gabrieldominguezduran@gmail.com>
2022-02-28 15:04:17 +0100

    Set up test env

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,23 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import Coin from "./models/coin";
+import CoinList from "./components/CoinsList";
+
+test("renders Coin's Charts title", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Coin's Charts/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// const fakeData: Coin = {
+//   icon: "Icon example",
+//   id: "fakeID",
+//   marketCap: 100,
+//   name: "fakeCoin",
+//   price: 150,
+//   priceBtc: 0.10,
+//   priceChange1d: -0.5,
+//   symbol: "fakeSymbol",
+//   volume: 50,
+//   };
